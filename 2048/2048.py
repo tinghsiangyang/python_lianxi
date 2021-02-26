62bdad40a52f295d680d64c10a08ed868789991a
tinghsiangyang <tinghsiangyang@qq.com>
2021-02-26 23:11:24 +0800

    2048第一天

---
+++ 2048/2048.py
@@ -0,0 +1,4 @@
+import curses # 该模块用来在终端上显示图形界面
+from random import randrange, choice # random 模块用来生成随机数
+from collections import defaultdict # collection 提供了一个字典dict的子类 defaultdict，可以指定 key 的之不存在时，value 的默认值
+
